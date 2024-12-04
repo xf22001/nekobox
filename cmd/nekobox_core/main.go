380f1808d89266ec5031ae5ae0d334e40b4c5cd9
xiaofei <alibaba22001@163.com>
2024-12-04 16:58:38 +0800

    update

--- cmd/nekobox_core/main.go
+++ cmd/nekobox_core/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"libneko/neko_common"
 	"os"
 	_ "unsafe"
 
@@ -15,7 +14,6 @@ func main() {
 	fmt.Println()
 	// nekobox_core
 	if len(os.Args) > 1 && os.Args[1] == "nekobox" {
-		neko_common.RunMode = neko_common.RunMode_NekoBox_Core
 		grpc_server.RunCore(setupCore, &server{})
 		return
 	}
