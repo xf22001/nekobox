e7a8c3f47a3274d6974b5af29e10ca9ae14cd623
xiaofei <alibaba22001@163.com>
2024-12-05 11:22:27 +0800

    update

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@
 #   文件名称：compile.sh
 #   创 建 者：肖飞
 #   创建日期：2024年12月04日 星期三 15时14分18秒
-#   修改日期：2024年12月04日 星期三 16时27分25秒
+#   修改日期：2024年12月05日 星期四 11时22分06秒
 #   描    述：
 #
 #================================================================
@@ -16,7 +16,7 @@ function main() {
 	export GOARCH=amd64
 	export TAGS_GO120="with_gvisor,with_dhcp,with_wireguard,with_reality_server,with_clash_api,with_quic,with_utls"
 	export TAGS_GO121="with_ech"
-	export TAGS="$TAGS_GO118,$TAGS_GO120,$TAGS_GO121"
+	export TAGS="$TAGS_GO120,$TAGS_GO121"
 
 	pushd sing-box
 	export GOHOSTOS="$(go env GOHOSTOS)"
