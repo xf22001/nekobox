21429db8d40dd3006752c9fdbd5e0d81d1d7cb3b
xiaofei <alibaba22001@163.com>
2026-01-16 11:47:47 +0800

    update

--- libneko/boxapi/go_http.go
+++ libneko/boxapi/go_http.go
@@ -11,8 +11,12 @@ import (
 
 func CreateProxyHttpClient(box *box.Box) *http.Client {
 	transport := &http.Transport{
-		TLSHandshakeTimeout:   time.Second * 3,
-		ResponseHeaderTimeout: time.Second * 3,
+		TLSHandshakeTimeout:   time.Second * 5,      // 增加 TLS 握手超时
+		ResponseHeaderTimeout: time.Second * 10,     // 增加响应头超时
+		ExpectContinueTimeout: time.Second * 1,      // 保持期望继续超时
+		IdleConnTimeout:       time.Second * 30,     // 增加空闲连接超时
+		MaxIdleConns:          100,                  // 保持合理的最大空闲连接数
+		MaxIdleConnsPerHost:   10,                   // 保持每主机最大空闲连接数
 	}
 
 	if box != nil {
@@ -23,6 +27,7 @@ func CreateProxyHttpClient(box *box.Box) *http.Client {
 
 	client := &http.Client{
 		Transport: transport,
+		Timeout:   time.Second * 60,                 // 增加总超时时间
 	}
 
 	return client
